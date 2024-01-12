e22d6d78c1a2c58242b3821e98009bb8e58b9f00
Marcelo <marcebq2005@gmail.com>
2024-01-12 02:35:58 -1200

    listo al 95%

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+//Seleccionar el body
+const BODY_ELEMENT = document.body
+
+
 //Seleccionar Formularios
 const BOX_ONE = document.querySelector('.box-one')
 const BOX_TWO = document.querySelector('.box-two')
@@ -10,17 +14,19 @@ const BOX_THREE = document.querySelector('.box-three')
 const NAME_LABEL = document.querySelector('#name')
 const EMAIL_LABEL = document.querySelector('#email')
 
-
 //Almacenar Valores
 let nameValue = ' '
 let emailValue = ' '
+let bottonValue = ' '
 
 //Seleccionar Botones
 const BTN_FIRST = document.querySelector('#btn-first')
 const BTN_SECOND = document.querySelector('#btn-second')
 
+
 // ------ Segundo Step ----- //
 const BOTTONS_SELECTED = document.querySelectorAll('.option')
+
 
 // ------ Tercer Step ----- //
 const THREE_CONTAINER = document.querySelector('.third-register-container')
@@ -30,10 +36,17 @@ const NAME_ELEMENT = document.createElement('p')
 const EMAIL_ELEMENT = document.createElement('p')
 const LIST_ELEMENT = document.createElement('ul')
 
+//Crear elemento para el step
+const NUM_ELEMENT = document.createElement('p')
+
+
 //Tomar valor dates container
 const DATE_CONTAINER = document.querySelector('.dates')
 
-
+//Para el apartado del step
+let numValue = 1;
+NUM_ELEMENT.innerHTML = `Step ${numValue} of 3`
+BODY_ELEMENT.appendChild(NUM_ELEMENT)
 
 //funciones para cambiar primer boton 
 BTN_FIRST.addEventListener('click', (e)=>{
@@ -54,14 +67,15 @@ BTN_FIRST.addEventListener('click', (e)=>{
         console.log('Nombre:', nameValue);
         console.log('Email:', emailValue);
 
+        NUM_ELEMENT.innerHTML = `Step ${numValue + 1} of 3`
+
     } else {
         alert("Algunos campos no son válidos. Por favor, revisa el formulario.");
     }
 
+    
 });
 
-
-let bottonValue = ' '
 
 // Funcion tomar los valores del boton
 BOTTONS_SELECTED.forEach(botton=>{
@@ -89,8 +103,14 @@ BTN_SECOND.addEventListener('click', (e)=>{
 
     BOX_TWO.classList.toggle('add-form')
     BOX_THREE.classList.toggle('add-form')
+
+    NUM_ELEMENT.innerHTML = `Step ${numValue + 2} of 3`
    
 })
+
+
+
+
 
 
 
